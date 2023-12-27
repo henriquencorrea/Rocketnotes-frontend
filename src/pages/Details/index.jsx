ea49461ea0d8174fec446bd949c34f136b10731f
Carlos Henrique Nunes Correa <carloscorrea.dpss@gmail.com>
2023-12-27 11:32:06 -0300

    Created 'section' component and added the 'Useful Links' section

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,22 @@
-import { Container } from './styles.js';
+import { Container, Links } from './styles.js';
 
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
+import { Section } from '../../components/Section';
 
 export function  Details() {
 
   return (
     <Container>
       <Header />
+
+      <Section title="Links úteis">
+        <Links>
+          <li> <a href='#'>https://rocketseat.com</a></li>
+          <li> <a href='#'>https://rocketseat.com</a></li>
+        </Links>
+
+      </Section>
 
       <Button  title="Voltar" />
       
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -11,3 +11,17 @@ import styled from "styled-components";
   "content";
 
 `;
+
+export const Links = styled.ul`
+  list-style: none;
+
+  > li {
+    margin-top: 12px;
+
+    a {
+      color: ${({ theme }) => theme.COLORS.WHITE};
+    }
+  }
+
+
+`;
