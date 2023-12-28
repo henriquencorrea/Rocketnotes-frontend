4a6c636cda141039b5cc935b228c156212a862ca
Carlos Henrique Nunes Correa <carloscorrea.dpss@gmail.com>
2023-12-28 17:51:27 -0300

    Added and styled user Avatar and Form

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,7 +1,7 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi'
-import { Container, Form } from './styles.js'
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
+import { Container, Form, Avatar } from './styles.js'
 
 
 
@@ -10,9 +10,20 @@ export function Profile(){
     <Container>
       <header>
         <a href="/"><FiArrowLeft /></a>
+  
       </header>
 
       <Form>
+
+        <Avatar>
+          <img src="https://github.com/Henriquencorrea.png" alt="Foto do usuário" />
+
+          <label htmlFor="avatar">
+          <FiCamera />
+          <input id="avatar" type="file"/>
+          </label>
+        </Avatar>
+        
         <Input
           placeholder="Nome"
           type="text"
