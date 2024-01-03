e3949eca98b12837c706f2c3321dab6b3a7de7b7
Carlos Henrique Nunes Correa <carloscorrea.dpss@gmail.com>
2024-01-03 12:09:58 -0300

    Created a method to share user data and a function for authentication

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { FiMail, FiLock } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
@@ -9,10 +10,17 @@ import { Button } from '../../components/Button';
 import { Container, Form, Background } from './styles';
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const data = useAuth();
-  console.log("MEU CONTEXTO =>", data);
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password });
+
+  }
   
+
   return (
 <Container>
   <Form>
@@ -25,14 +33,18 @@ export function SignIn() {
         placeholder="Email"
         type="text"
         icon={FiMail}
+        onChange={e => setEmail(e.target.value)}
       />
+
        <Input
         placeholder="Senha"
         type="password"
         icon={FiLock}
+        onChange={e => setPassword(e.target.value)}
+
       />
 
-      <Button title="Entrar" />
+      <Button title="Entrar" onClick={handleSignIn} />
 
       <Link to="/register">
       Criar conta
