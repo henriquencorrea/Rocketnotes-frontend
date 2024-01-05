ef4605b54c8e52a2425192b38c4245b4b670cb51
Carlos Henrique Nunes Correa <carloscorrea.dpss@gmail.com>
2024-01-05 10:56:25 -0300

    Added method to save a new link in the New Notes interface.

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Link } from 'react-router-dom';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
@@ -10,6 +12,15 @@ import { Button } from '../../components/Button';
 import { Container, Form } from './styles';
 
 export function New() {
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState("");
+
+  function handleAddLink() {
+    setLinks(prevState => [...prevState, newLink]);
+    setNewLink("");
+  }
+
+
   return (
     <Container>
       <Header />
@@ -25,8 +36,22 @@ export function New() {
           <Textarea placeholder="Observações"/>
 
           <Section title="Links úteis">
-            <NoteItem value="https://rocketseat.com.br"/>
-            <NoteItem isNew placeholder="Novo link"/>
+            {
+              links.map((link, index) => (
+                <NoteItem
+                  key={String(index)}
+                  value={link}
+                  onClick={() => { }}
+                />
+              ))
+            }
+            <NoteItem
+              isNew
+              placeholder="Novo link"
+              value={newLink}
+              onChange={e => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+            />
           </Section>
 
           <Section title="Marcadores">
