0695efacbb3493d9859b91ef13eef36883a07cf5
Carlos Henrique Nunes Correa <carloscorrea.dpss@gmail.com>
2024-01-09 20:21:43 -0300

    Adjusting unavailable routes.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,22 @@
-import { RiShutDownLine } from 'react-icons/ri'
 import { useAuth } from '../../hooks/auth'
+import { useNavigate } from 'react-router-dom';
 import { api } from '../../services/api';
+
+import { RiShutDownLine } from 'react-icons/ri'
 import { Container, Profile, Logout } from "./styles";
 
 import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
 
 export function Header() {
   const { signOut, user } = useAuth();
+  const navigation = useNavigate();
+
+  function handleSignOut() {
+    navigation("/");
+    signOut();
+
+
+  }
 
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
@@ -22,7 +32,7 @@ export function Header() {
         </div>
       </Profile>
 
-        <Logout onClick={signOut}>
+        <Logout onClick={handleSignOut}>
           <RiShutDownLine />
         </Logout>
 
